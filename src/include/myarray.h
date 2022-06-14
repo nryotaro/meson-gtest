449a7dca52289f9a01b830918525b98bec85d99b
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2022-06-14 18:03:05 +0900

    Setup the scaffold of myarray.h

--- src/include/myarray.h
+++ src/include/myarray.h
@@ -1,3 +1,27 @@
+#ifndef MYARRAY_H_
+#define MYARRAY_H_
+#include <cstddef>
+#include <initializer_list>
+#include <memory>
 
+namespace myarray {
+template <typename T> class MyArray {
+public:
+  MyArray(std::initializer_list<T> list) {
+    size = capacity = list.size();
+    ptr = std::make_unique<T[]>(capacity);
+    T *p = ptr.get();
+    int i = 0;
+    for (T item : list)
+      *(p + i++) = item;
+  }
 
-int abc();
+  const T &operator[](size_t i) { return ptr[i]; }
+
+private:
+  std::unique_ptr<T[]> ptr;
+  size_t capacity;
+  size_t size;
+};
+} // namespace myarray
+#endif
--- tests/myarray.cc
+++ tests/myarray.cc
@@ -1,4 +1,9 @@
 #include "myarray.h"
 #include <gtest/gtest.h>
+using namespace myarray;
 
-TEST(a, 1) { EXPECT_EQ(1, abc()); }
+TEST(ArrayAccess, 1) {
+  MyArray<int> sut = {23, 2};
+
+  EXPECT_EQ(sut[1], 2);
+}
